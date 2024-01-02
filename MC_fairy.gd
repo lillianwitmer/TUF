c9b669cc9ea6ec67a28d768250423d6bf8c0ca58
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2024-01-02 12:49:53 -0600

    new yearrrrrrrrrrrrrrrrrrrrr

---
+++ MC_fairy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+enum STATES { IDLE = 0, DEAD, DAMAGED, ATTACKING, DASHING }
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func update_animation(direction):
+	if data.state == STATES.IDLE:
+		
