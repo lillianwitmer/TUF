a9bc17315dbe989089116beed954f892605ab716
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2024-01-03 12:57:26 -0600

    lollol2024

--- MC_fairy.gd
+++ MC_fairy.gd
@@ -6,8 +6,36 @@ const JUMP_VELOCITY = -400.0
 
 enum STATES { IDLE = 0, DEAD, DAMAGED, ATTACKING, DASHING }
 
+@export var data = {
+	"max_health": 60.0,  # 20hp per heart; 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
+	"state": STATES.IDLE,
+	"secondaries": [],
+}
+func get_direction_name():
+	return ["right", "down", "left", "up"][
+		int(round(look_direction.angle() * 2 / PI)) % 4
+]
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var look_direction = Vector2.RIGHT
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
 func _physics_process(delta):
 	
@@ -27,5 +55,4 @@ func _physics_process(delta):
 	move_and_slide()
 
 func update_animation(direction):
-	if data.state == STATES.IDLE:
-		
+
