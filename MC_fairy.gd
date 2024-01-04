5d4f0538416d41c93deed1c419b84550a31e63fc
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2024-01-04 12:51:38 -0600

    lll

--- MC_fairy.gd
+++ MC_fairy.gd
@@ -19,23 +19,34 @@ func get_direction_name():
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var look_direction = Vector2.RIGHT
+var screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
+	var velocity = Vector2.ZERO 
+	if Input.is_action_pressed("ui_left"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * SPEED
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
 
 func _physics_process(delta):
 	
@@ -52,7 +63,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	get_input()
 	move_and_slide()
 
-func update_animation(direction):
+
 
